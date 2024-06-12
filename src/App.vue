3c51965c186ce22e1aa46f64ee402f30cf44dcba
rachelpatrocinio <rachelannepatrocinio@gmail.com>
2024-06-12 16:07:16 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+  export default {
+    data(){
+      return{
+
+      }
+    },
+    methods:{
+
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
