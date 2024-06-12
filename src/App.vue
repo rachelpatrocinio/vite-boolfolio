85cfda1a69796a90f18bc00ae6346b6fd0177fed
rachelpatrocinio <rachelannepatrocinio@gmail.com>
2024-06-12 16:17:52 +0200

    Added: took projects data from DB thru axios and printed it on page

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
   <div>
+    <ul>
+      <li v-for="project in projects">
+        <h5>{{ project.project_title }}</h5>
+        <p>{{ project.project_description }}</p>
+      </li>
+    </ul>
 
   </div>
 </template>
@@ -9,15 +15,24 @@ import axios from 'axios';
   export default {
     data(){
       return{
-
+        projects: []
       }
     },
     methods:{
-
+      fetchProjects(){
+        axios
+          .get('http://127.0.0.1:8000/api/projects')
+          .then(res =>{
+            // console.log(res.data.projects);
+            this.projects = res.data.projects;
+          })
+      }
+    },
+    created(){
+      this.fetchProjects();
     }
   }
 </script>
 
 <style lang="scss" scoped>
-
 </style>
