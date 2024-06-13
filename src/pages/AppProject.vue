0f3385041d4d376faaf48dce49be26ee675ce98a
rachelpatrocinio <rachelannepatrocinio@gmail.com>
2024-06-13 17:56:25 +0200

    Added: datas of single project

--- src/pages/AppProject.vue
+++ src/pages/AppProject.vue
@@ -2,14 +2,41 @@
     <div class="container">
         <div class="row">
             <h3>{{ $route.params.slug }}</h3>
+
+            <div class="card">
+                <div class="card-title">
+                    <h2>{{ project.project_title }}</h2>
+                    <p>{{ project.project_description }}</p>
+                </div>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
-    export default {
-        
+import axios from 'axios';
+
+export default {
+    data(){
+        return{
+            project: null,
+            slug: this.$route.params.slug
+        }
+    },
+    methods:{
+        fetchProject(){
+            axios
+                .get("http://127.0.0.1:8000/api/projects/" + this.slug)
+                .then(res =>{
+                    console.log(res.data.project);
+                    this.project = res.data.project;
+                })
+        }
+    },
+    created(){
+        this.fetchProject()
     }
+}
 </script>
 
 <style lang="scss" scoped>
